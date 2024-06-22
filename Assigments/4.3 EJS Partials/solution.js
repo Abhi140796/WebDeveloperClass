77269d4a28c614663152c7b66c11b3e3cd19e9a9
Abhishek dubey <abhishekd905@gmail.com>
2024-06-23 01:59:21 +0530

    EJS Patials Code Improvement

--- Assigments/4.3 EJS Partials/solution.js
+++ Assigments/4.3 EJS Partials/solution.js
@@ -2,17 +2,17 @@ import express from "express";
 
 const app = express();
 const port = 3000;
-app.use(express.static("public"));
+app.use(express.static("D:/WebDeveloperClass/Assigments/4.3 EJS Partials/public"));
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("D:/WebDeveloperClass/Assigments/4.3 EJS Partials/views/index.ejs");
 });
 
 app.get("/about", (req, res) => {
-  res.render("about.ejs");
+  res.render("D:/WebDeveloperClass/Assigments/4.3 EJS Partials/views/about.ejs");
 });
 
 app.get("/contact", (req, res) => {
-  res.render("contact.ejs");
+  res.render("D:/WebDeveloperClass/Assigments/4.3 EJS Partials/views/contact.ejs");
 });
 
 app.listen(port, () => {
